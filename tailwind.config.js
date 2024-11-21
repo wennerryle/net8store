d1075bd186ce14e5c1222dc8df3ce9f08cabf2cf
Vladislav <wennerryle@gmail.com>
2024-11-21 17:42:14 +0300

    bump vesion net8.0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+//  Copyright (c) 2024 Vladislav Pozdeev. All rights reserved.
+//  Use this source code is governed by an MIT license
+//  that can be found in the LICENSE file.
+
+module.exports = {
+  content: ["./Pages/**/*.cshtml"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
