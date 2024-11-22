271aaa98fa14988a956b4b781c2f1956d2baaa5e
Vladislav <wennerryle@gmail.com>
2024-11-22 12:46:51 +0300

    chore: remove generated styles

--- websrc/components/ProductCard.ts
+++ websrc/components/ProductCard.ts
@@ -1,3 +1,7 @@
+//  Copyright (c) 2024 Vladislav Pozdeev. All rights reserved.
+//  Use this source code is governed by an MIT license
+//  that can be found in the LICENSE file.
+
 import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import {
--- websrc/index.ts
+++ websrc/index.ts
@@ -1,3 +1,6 @@
-console.log("everything is ok")
+//  Copyright (c) 2024 Vladislav Pozdeev. All rights reserved.
+//  Use this source code is governed by an MIT license
+//  that can be found in the LICENSE file.
+
 
 import "./components";
