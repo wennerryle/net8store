a65703f4339dbe0abc11770a12ce488dcc86fb6c
Vladislav <wennerryle@gmail.com>
2024-11-29 05:04:22 +0300

    feat[dx]: add swagger

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using net8store.Core;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,17 +11,35 @@ builder.Services.AddDbContext<StoreContext>(options => {
     options.UseSqlite();
 });
 
+builder.Services.AddMvcCore();
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductStoreAPI", Version = "v1" });
+});
+
 var app = builder.Build();
 
+var isDevelopment = app.Environment.IsDevelopment();
+
+Console.WriteLine($"IsDevelopment: {isDevelopment}");
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!isDevelopment) {
     app.UseExceptionHandler("/Error");
+} else {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapControllers();
 
 app.UseAuthorization();
 
